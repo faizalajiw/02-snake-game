ca9a3a772cfc8ed7dbe9b84d2ef9ca5d7d150317
faizalajiw <faizalajiwibowo7@gmail.com>
2022-03-13 23:06:30 +0700

    fix karakter ular, icon nyawa, level, sound

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-const CELL_SIZE = 30;
+const CELL_SIZE = 20;
 const CANVAS_SIZE = 600;
 const REDRAW_INTERVAL = 50;
 const WIDTH = CANVAS_SIZE / CELL_SIZE;
 const HEIGHT = CANVAS_SIZE / CELL_SIZE;
 const DIRECTION = {
-        LEFT: 0,
-        RIGHT: 1,
-        UP: 2,
-        DOWN: 3,
-    }
-    // Pengaturan Speed (semakin kecil semakin cepat) ubah dari 150 ke 120
-const MOVE_INTERVAL = 120;
+    LEFT: 0,
+    RIGHT: 1,
+    UP: 2,
+    DOWN: 3,
+}
 
-// let nyawasnake = 3;
+// const MOVE_INTERVAL = 120;
+let move_interval;
 
 function initPosition() {
     return {
@@ -23,7 +22,7 @@ function initPosition() {
 
 function initHeadAndBody() {
     let head = initPosition();
-    let body = [{ x: head.x, y: head.y }];
+    let body = [{x: head.x, y: head.y}];
     return {
         head: head,
         body: body,
@@ -39,105 +38,189 @@ function initSnake(color) {
         color: color,
         ...initHeadAndBody(),
         direction: initDirection(),
-        // nyawasnake: 3,
         score: 0,
     }
 }
+let snake = initSnake("blue");
 
-// deklarasi karakter ular
-let snake1 = {
-    color: "blue",
-    ...initHeadAndBody(),
-    direction: initDirection(),
-    score: 0,
-}
-let snake2 = {
-    color: "yellow",
-    ...initHeadAndBody(),
-    direction: initDirection(),
-    score: 0,
-}
-let snake3 = {
-    color: "red",
-    ...initHeadAndBody(),
-    direction: initDirection(),
-    score: 0,
-}
-
-// let nyawa = {
-//     position: initPosition()
-// }
-
-// Atur Karakter1
-const head1 = new Image();
-head1.src = "./assets/head1.png";
-const body1 = new Image();
-body1.src = "./assets/body1.png";
-
-// Atur Karakter 2
-const head2 = new Image();
-head2.src = "./assets/head2.png";
-const body2 = new Image();
-body2.src = "./assets/body2.png";
-
-// Atur Karakter 3
-const head3 = new Image();
-head3.src = "./assets/head3.png";
-const body3 = new Image();
-body3.src = "./assets/body3.png";
-
-// deklarasi karakter apel
 let apples = [{
-        color: "red",
-        position: initPosition(),
+    position: initPosition(),
+},
+{
+    position: initPosition(),
+}];
+
+let life = 3;
+let drink = {
+    position: initPosition(),
+};
+
+let level = [
+    {
+        speed: 120,
     },
     {
-        color: "green",
-        position: initPosition(),
+        speed: 110,
+        wall: [
+            {
+                start: {
+                    x: 5,
+                    y: 15,
+                },
+                end: {
+                    x: 25,
+                    y: 15,
+                }
+            }
+        ],
+    },
+    {
+        speed: 100,
+        wall: [
+            {
+                start: {
+                    x: 5,
+                    y: 12,
+                },
+                end: {
+                    x: 25,
+                    y: 12,
+                }
+            },
+            {
+                start: {
+                    x: 5,
+                    y: 18,
+                },
+                end: {
+                    x: 25,
+                    y: 18,
+                }
+            }
+        ],
+    },
+    {
+        speed: 90,
+        wall: [
+            {
+                start: {
+                    x: 0,
+                    y: 15,
+                },
+                end: {
+                    x: 30,
+                    y: 15,
+                }
+            },
+            {
+                start: {
+                    x: 15,
+                    y: 0,
+                },
+                end: {
+                    x: 15,
+                    y: 15,
+                }
+            }
+        ],
+    },
+    {
+        speed: 80,
+        wall: [
+            {
+                start: {
+                    x: 0,
+                    y: 15,
+                },
+                end: {
+                    x: 30,
+                    y: 15,
+                }
+            },
+            {
+                start: {
+                    x: 15,
+                    y: 0,
+                },
+                end: {
+                    x: 15,
+                    y: 30,
+                }
+            }
+        ],
     }
-]
+];
+let current_level = 0;
+let next_level = 1;
 
 function drawCell(ctx, x, y, color) {
     ctx.fillStyle = color;
     ctx.fillRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
 }
 
-// nyawa board
-// function drawNyawa(snake) {
-//     let NyawaCanvas;
-//     NyawaCanvas = document.getElementById("nyawaBoard");
-//     let NyawaCtx = NyawaCanvas.getContext("2d");
-//     let nyawaX = 10;
-//     let nyawaY = 5;
-//     let cell = 15;
-//     NyawaCtx.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
-//     for (let i = 1; i <= snake.nyawa; i++) {
-//         var img = document.getElementById("nyawa");
-//         if (i % 11 == 0) {
-//             nyawaY += 25;
-//             nyawaX = 10
-//         }
-//         NyawaCtx.drawImage(img, nyawaX, nyawaY, cell, cell);
-//         nyawaX += 20;
-//     }
-// }
+function drawWall(ctx, wall) {
+    for (let i = 0; i < wall.length; i++) {
+        let start = wall[i].start;
+        let end = wall[i].end;
 
-// Untuk menampilkan score board
-function drawScore(snake) {
-    let scoreCanvas;
-    if (snake.color == snake1.color) {
-        scoreCanvas = document.getElementById("score1Board");
-    } else if (snake.color == snake2.color) {
-        scoreCanvas = document.getElementById("score2Board");
-    } else {
-        scoreCanvas = document.getElementById("score3Board");
+        if (start.x == end.x) {
+            ctx.fillRect(start.x * CELL_SIZE, start.y * CELL_SIZE, CELL_SIZE, (end.y - start.y) * CELL_SIZE);
+        } else if (start.y == end.y) {
+            ctx.fillRect(start.x * CELL_SIZE, start.y * CELL_SIZE, (end.x - start.x) * CELL_SIZE, CELL_SIZE);
+        }
     }
-    let scoreCtx = scoreCanvas.getContext("2d");
+}
 
-    scoreCtx.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
-    scoreCtx.font = "30px Arial";
-    scoreCtx.fillStyle = snake.color
-    scoreCtx.fillText(snake.score, 10, scoreCanvas.scrollHeight / 2);
+function insertImage(ctx, x, y, name) {
+    let image = document.createElement("img");
+    image.src = `assets/${name}.png`;
+
+    ctx.drawImage(image, x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+}
+
+function getCurrentLevel(ctx, score) {
+
+    if (score > 25) {
+        ctx.fillText(`WIN`, 500, 580);
+        return;
+    }
+
+    if (score >= 0 && score < 5) {
+        current_level = 0;
+    } else if (score >= 5 && score < 10) {
+        current_level = 1;
+    } else if (score >= 10 && score < 15) {
+        current_level = 2;
+    } else if (score >= 15 && score < 20) {
+        current_level = 3;
+    } else if (score >= 20 && score < 25) {
+        current_level = 4;
+    }
+
+    if (current_level == next_level) {
+        next_level++;
+        let audio = new Audio('assets/level-up.mp3');
+        audio.play();
+    }
+
+    move_interval = level[current_level].speed;
+    ctx.fillText(`Level : ${current_level + 1}`, 500, 580);
+    if (current_level > 0) {
+        drawWall(ctx, level[current_level].wall);
+    }
+}
+
+function checkPrime(number) {
+    if (number < 2) {
+        return false;
+    }
+
+    for (let i = 2; i < number; i++) {
+        if (number % i == 0) {
+            return false;
+        }
+    }
+    return true;
 }
 
 function draw() {
@@ -146,49 +229,35 @@ function draw() {
         let ctx = snakeCanvas.getContext("2d");
 
         ctx.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
-        // karakter ular1
-        ctx.drawImage(head1, snake1.head.x * CELL_SIZE, snake1.head.y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
-        for (let i = 1; i < snake1.body.length; i++) {
-            ctx.drawImage(body1, snake1.body[i].x * CELL_SIZE, snake1.body[i].y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+
+        insertImage(ctx, snake.head.x, snake.head.y, "head1");
+
+        for (let i = 1; i < snake.body.length; i++) {
+            insertImage(ctx, snake.body[i].x, snake.body[i].y, "body1");
         }
 
-        // karakter ular2
-        ctx.drawImage(head2, snake2.head.x * CELL_SIZE, snake2.head.y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
-        for (let i = 1; i < snake2.body.length; i++) {
-            ctx.drawImage(body2, snake2.body[i].x * CELL_SIZE, snake2.body[i].y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
-        }
+        ctx.color = snake.color;
+        ctx.font = "20px Arial";
 
-        // karakter ular3
-        ctx.drawImage(head3, snake3.head.x * CELL_SIZE, snake3.head.y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
-        for (let i = 1; i < snake3.body.length; i++) {
-            ctx.drawImage(body3, snake3.body[i].x * CELL_SIZE, snake3.body[i].y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
-        }
+        getCurrentLevel(ctx, snake.score);
+        ctx.fillText("Score : "+ snake.score, 485, 30);
+        ctx.fillText("Speed : "+ level[current_level].speed, 485, 50);
 
-        // karakter 2 apel
         for (let i = 0; i < apples.length; i++) {
             let apple = apples[i];
-            var img = document.getElementById("apple");
-            ctx.drawImage(img, apple.position.x * CELL_SIZE, apple.position.y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+            insertImage(ctx, apple.position.x, apple.position.y, "apple");
         }
 
-        // let prima = 0;
-        // for (let k = 1; k <= snake.score; k++) {
-        //     if (snake.score % k == 0) {
-        //         prima++;
-        //     }
-        // }
-        // if (prima == 2) {
-        //     var img = document.getElementById("nyawa");
-        //     ctx.drawImage(img, nyawa.position.x * CELL_SIZE, nyawa.position.y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
-        // }
+        for (let i = 0; i < life; i++) {
+            insertImage(ctx, i, 0, "heart");
+        }
 
-        drawScore(snake1);
-        drawScore(snake2);
-        drawScore(snake3);
-        // drawNyawa(nyawa);
+        if (checkPrime(snake.score)) {
+            insertImage(ctx, drink.position.x, drink.position.y, "drink");
+        }
     }, REDRAW_INTERVAL);
 }
-
+// agar ular bisa tembus
 function teleport(snake) {
     if (snake.head.x < 0) {
         snake.head.x = CANVAS_SIZE / CELL_SIZE - 1;
@@ -204,67 +273,86 @@ function teleport(snake) {
     }
 }
 
-// apples array
-function eat(snake, apples) {
-    var audio = new Audio('./assets/makan.mp3')
+function eat(snake, apples, drink) {
+    // Apel
     for (let i = 0; i < apples.length; i++) {
         let apple = apples[i];
+        let AUDIO_EAT = new Audio('assets/eat.mp3');
         if (snake.head.x == apple.position.x && snake.head.y == apple.position.y) {
-            audio.play();
             apple.position = initPosition();
             snake.score++;
-            snake.body.push({ x: snake.head.x, y: snake.head.y });
+            snake.body.push({x: snake.head.x, y: snake.head.y});
+            AUDIO_EAT.play();
         }
+    }
+    // Poison
+    if (snake.head.x == drink.position.x && snake.head.y == drink.position.y) {
+        let AUDIO_EAT = new Audio('assets/.mp3');
+        drink.position = initPosition();
+        snake.score++;
+        snake.body.push({x: snake.head.x, y: snake.head.y});
+        AUDIO_EAT.play();
+        life++;
     }
 }
 
 function moveLeft(snake) {
     snake.head.x--;
     teleport(snake);
-    eat(snake, apples);
+    eat(snake, apples, drink);
 }
 
 function moveRight(snake) {
     snake.head.x++;
     teleport(snake);
-    eat(snake, apples);
+    eat(snake, apples, drink);
 }
 
 function moveDown(snake) {
     snake.head.y++;
     teleport(snake);
-    eat(snake, apples);
+    eat(snake, apples, drink);
 }
 
 function moveUp(snake) {
     snake.head.y--;
     teleport(snake);
-    eat(snake, apples);
+    eat(snake, apples, drink);
 }
-// Karakter tewas
-function checkCollision(snakes) {
+
+function checkCollision() {
     let isCollide = false;
-    //this
-    for (let i = 0; i < snakes.length; i++) {
-        for (let j = 0; j < snakes.length; j++) {
-            for (let k = 1; k < snakes[j].body.length; k++) {
-                if (snakes[i].head.x == snakes[j].body[k].x && snakes[i].head.y == snakes[j].body[k].y) {
-                    isCollide = true;
-                }
+
+    // cek collision antara snake head dan snake body
+    for (let i = 1; i < snake.body.length; i++) {
+        if (snake.head.x == snake.body[i].x && snake.head.y == snake.body[i].y) {
+            isCollide = true;
+        }
+    }
+
+    // cek collision antara snake head dan wall
+    if (current_level > 0) {
+        let wall = level[current_level].wall;
+        for (let i = 0; i < wall.length; i++) {
+            if (snake.head.x >= wall[i].start.x && snake.head.x <= wall[i].end.x && snake.head.y >= wall[i].start.y && snake.head.y <= wall[i].end.y) {
+                isCollide = true;
             }
         }
     }
-    if (isCollide) {
-        // game over audio:
-        var audio = new Audio('./assets/game-over.mp3');
-        audio.play();
 
-        alert("Game over");
-        snake1 = initSnake("blue");
-        snake2 = initSnake("yellow");
-        snake3 = initSnake("red");
+    if (isCollide) {
+        life--;
+        snake.body.splice(0, snake.body.length);
+        snake.body.push({x: snake.head.x, y: snake.head.y});
+        // sound game over
+        if (life == 0) {
+            let audio = new Audio('assets/game-over.mp3');
+            audio.play();
+
+            alert("Game over");
+            snake = initSnake("purple");
+        }
     }
-    return isCollide;
 }
 
 function move(snake) {
@@ -283,21 +371,23 @@ function move(snake) {
             break;
     }
     moveBody(snake);
-    // Check collision dengan snake3
-    if (!checkCollision([snake1, snake2, snake3])) {
+    checkCollision();
+
+    if (life > 0) {
         setTimeout(function() {
             move(snake);
-        }, MOVE_INTERVAL);
+        }, move_interval);
     } else {
+        life = 3;
         initGame();
     }
 }
-
+// menambah badan
 function moveBody(snake) {
     snake.body.unshift({ x: snake.head.x, y: snake.head.y });
     snake.body.pop();
 }
-
+// mengatur navigasi ular
 function turn(snake, direction) {
     const oppositeDirections = {
         [DIRECTION.LEFT]: DIRECTION.RIGHT,
@@ -310,44 +400,22 @@ function turn(snake, direction) {
         snake.direction = direction;
     }
 }
-
-// Mengatur kendali snake1
-document.addEventListener("keydown", function(event) {
+// mengatur navigasi ular
+document.addEventListener("keydown", function (event) {
     if (event.key === "ArrowLeft") {
-        turn(snake1, DIRECTION.LEFT);
+        turn(snake, DIRECTION.LEFT);
     } else if (event.key === "ArrowRight") {
-        turn(snake1, DIRECTION.RIGHT);
+        turn(snake, DIRECTION.RIGHT);
     } else if (event.key === "ArrowUp") {
-        turn(snake1, DIRECTION.UP);
+        turn(snake, DIRECTION.UP);
     } else if (event.key === "ArrowDown") {
-        turn(snake1, DIRECTION.DOWN);
+        turn(snake, DIRECTION.DOWN);
     }
-    // Mengatur kendali snake2
-    if (event.key === "a") {
-        turn(snake2, DIRECTION.LEFT);
-    } else if (event.key === "d") {
-        turn(snake2, DIRECTION.RIGHT);
-    } else if (event.key === "w") {
-        turn(snake2, DIRECTION.UP);
-    } else if (event.key === "s") {
-        turn(snake2, DIRECTION.DOWN);
-    }
-    // Mengatur kendali snake3
-    if (event.key === "j") {
-        turn(snake3, DIRECTION.LEFT);
-    } else if (event.key === "l") {
-        turn(snake3, DIRECTION.RIGHT);
-    } else if (event.key === "i") {
-        turn(snake3, DIRECTION.UP);
-    } else if (event.key === "k") {
-        turn(snake3, DIRECTION.DOWN);
-    }
+
 })
 
 function initGame() {
-    move(snake1);
-    move(snake2);
-    move(snake3);
+    move(snake);
 }
 
 initGame();
